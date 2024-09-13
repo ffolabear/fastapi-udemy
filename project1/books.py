99baf588048fd18959ad1926ef9767e5cc49902b
ffolabear <ffolabear@gmail.com>
2024-09-13 13:53:09 +0900

    feat : Project1 예제 추가

--- project1/books.py
+++ project1/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -12,7 +12,7 @@ BOOKS = [
     {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
     {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
     {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
-    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Fi  ve', 'category': 'math'},
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
@@ -45,3 +45,54 @@ async def read_specific_book(book_title: str):
         # casefold() : 소문자 변환
         if book.get('title').casefold() == book_title.casefold():
             return book
+
+
+# url 에 쿼리 파라미터를 정의하지 않아도 알아서 적용
+@app.get("/books/")
+async def read_category_by_query(category: str) -> list:
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+# 밑에 함수보다 밑에 있으면 오류남
+# ‼️작은 api 는 가급적 앞에 있어야함 ‼️
+@app.get("/books/author/")
+async def read_books_written_by(author: str) -> list:
+    books_to_return = []
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('author').casefold() == author.casefold():
+            books_to_return.append(BOOKS[i])
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str) -> list:
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+# swagger 에서 테스트할때는 쌍따음표를 써야함
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(update_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == update_book.get('title').casefold():
+            BOOKS[i] = update_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
