dcc809635bb614fb551cac094b8210d1fbc85537
ffolabear <ffolabear@gmail.com>
2024-09-17 23:58:15 +0900

    feat : Project2 예제 추가

--- project2/books2.py
+++ project2/books2.py
@@ -12,13 +12,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_year: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_year):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_year = published_year
 
 
 class BookRequest(BaseModel):
@@ -27,6 +29,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=3)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-0, lt=6)  # 최대, 최소 모두 경계값은 포함하지 않음
+    published_year: int = Field(gt=1999, lt=2099)
 
     model_config = {
         "json_schema_extra": {
@@ -34,25 +37,51 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "codingwithroby",
                 "description": "A description of new book",
-                "rating": 5
+                "rating": 5,
+                "published_year": 2012
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
-    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
-    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
-    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
-    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
-    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2012),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 2013),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5, 2014),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2016),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2018),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2023)
 ]
 
 
 @app.get("/books2")
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/published/")
+async def read_book_by_published_year(published_year: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_year == published_year:
+            books_to_return.append(book)
+    return books_to_return
 
 
 @app.post("/create-book")
@@ -69,3 +98,18 @@ def find_book_id(book: Book):
     #     book.id = 1
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
+
+
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
