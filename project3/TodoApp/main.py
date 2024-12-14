585627a68ecb0096943ffd7a3b63019c5140085b
ffolabear <ffolabear@gmail.com>
2024-12-14 23:59:29 +0900

    feat : Project3 예제 추가

--- project3/TodoApp/main.py
+++ project3/TodoApp/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 import models
 from database import engine
+from project3.TodoApp.database import SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
