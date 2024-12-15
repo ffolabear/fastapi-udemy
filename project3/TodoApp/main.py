8256bac669f664824df2d83a38e8410bef765517
ffolabear <ffolabear@gmail.com>
2024-12-15 23:55:53 +0900

    feat : Project3 예제 추가

--- project3/TodoApp/main.py
+++ project3/TodoApp/main.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Path
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from starlette import status
+
 import models
+from models import Todos
 from database import engine
-from project3.TodoApp.database import SessionLocal
+from database import SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     db = SessionLocal()
@@ -13,3 +21,32 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(models.Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found.')
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db:db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.dict())
+    db.add(todo_model)
+    db.commit()
