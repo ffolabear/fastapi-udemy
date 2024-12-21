e1ed22651ddd00e53c83ddae3564d5e757e59969
ffolabear <ffolabear@gmail.com>
2024-12-21 23:59:37 +0900

    feat : Project3 예제 추가

--- project3/TodoApp/routers/auth.py
+++ project3/TodoApp/routers/auth.py
@@ -22,7 +22,7 @@ async def create_user(create_user_request: CreateUserRequest):
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
 
