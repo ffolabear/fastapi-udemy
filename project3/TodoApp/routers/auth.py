ad05d6e602da436531429ed49128168295a6034d
ffolabear <ffolabear@gmail.com>
2024-12-24 23:59:06 +0900

    feat : Project3 예제 추가

--- project3/TodoApp/routers/auth.py
+++ project3/TodoApp/routers/auth.py
@@ -1,13 +1,14 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from passlib.context import CryptContext
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
 
 from database import SessionLocal
 from models import Users
-from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -34,6 +35,16 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
@@ -48,3 +59,9 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
 
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    return form_data.username
+
